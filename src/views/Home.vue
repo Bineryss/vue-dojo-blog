cb88b5bdf46452ff76396a71f5dbe39d9795e6a4
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2021-10-02 18:29:20 +0200

    using reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="home">
-    home
-  </div>
+	<div class="home">
+		<h1>Home</h1>
+		<p ref="p">{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+		<button @click="updateNinjaOne">Click Me</button>
+	</div>
 </template>
 
 <script>
-
+import { ref, reactive } from '@vue/reactivity'
 export default {
-  name: 'Home',
-  setup() {
-    
-  }
+	name: 'Home',
+	setup() {
+		const ninjaOne = reactive({ name: 'Ninja', age: 30 })
+
+		const updateNinjaOne = () => {
+			ninjaOne.age = 29
+		}
+
+		return {
+			ninjaOne,
+			updateNinjaOne,
+		}
+	},
 }
 </script>
