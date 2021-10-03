4fc8880f635744098b94493b540ef8c134ab54c8
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2021-10-03 18:18:37 +0200

    post components

---
+++ src/components/PostList.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="post-list"></div>
+	<div v-for="post in posts" :key="post.id">
+		<SinglePost :post="post" />
+	</div>
+</template>
+
+<script>
+import { onMounted } from '@vue/runtime-core'
+import SinglePost from '../components/SinglePost.vue'
+export default {
+	props: ['posts'],
+	components: {
+		SinglePost,
+	},
+	setup(props) {
+		onMounted(() => {
+			
+		})
+	},
+}
+</script>
+
+<style></style>
